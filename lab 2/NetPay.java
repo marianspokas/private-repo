f6d0ce737bef95517b3aeb1f753443e0016ea7d1
marianspokas <marianspokas@10.241.226.14>
2018-07-02 20:24:49 -0700

    take 2

--- lab 2/NetPay.java
+++ lab 2/NetPay.java
@@ -10,36 +10,35 @@
  * prof: Dr. Hao
  * import java util and create class, 'NetPay'
  */
-
-
 import java.util.*;
-public class NetPay {
 
+public class NetPay {
+	private static double FEDERAL_TAX_PERCENT = 0.10;
+	private static double STATE_TAX_PERCENT = 0.045;
+	private static double SS_PERCENT = 0.062;
+	private static double MEDICARE_PERCENT = 0.0145;
+	private static double PAY_PER_HOUR = 7.250;
+	
 	public static void main(String[] args) {
-		double FEDERAL_TAX_PERCENT = 0.10;
-		double STATE_TAX_PERCENT = 0.045;
-		double SS_PERCENT = 0.062;
-		double MEDICARE_PERCENT = 0.0145;
-		double PAY_PER_HOUR = 7.250;
 		double HOURS_PER_WEEK = 40.00;
-		double NET_PAY;
-		double GROSS_PAY;
+		double netPay;
+		double grossPay;
 		double DEDUCTIONS;
 		
 		System.out.println("Hours per week:" + "\t" + "\t" + HOURS_PER_WEEK);
 		
-		GROSS_PAY = (HOURS_PER_WEEK * PAY_PER_HOUR);
-		System.out.println("Gross pay:" + "\t" +"\t"  + "$" + GROSS_PAY );
+		grossPay = (HOURS_PER_WEEK * PAY_PER_HOUR);
+		System.out.println("Gross pay:" + "\t" +"\t"  + "$" + grossPay );
 		
-		FEDERAL_TAX_PERCENT = (GROSS_PAY * FEDERAL_TAX_PERCENT);
-		STATE_TAX_PERCENT = (GROSS_PAY * STATE_TAX_PERCENT);		
-		SS_PERCENT = (GROSS_PAY * SS_PERCENT);		
-		MEDICARE_PERCENT = (GROSS_PAY * MEDICARE_PERCENT);
+		FEDERAL_TAX_PERCENT = (grossPay * FEDERAL_TAX_PERCENT);
+		STATE_TAX_PERCENT = (grossPay * STATE_TAX_PERCENT);		
+		SS_PERCENT = (grossPay * SS_PERCENT);		
+		MEDICARE_PERCENT = (grossPay * MEDICARE_PERCENT);
 		
 		DEDUCTIONS = (FEDERAL_TAX_PERCENT + STATE_TAX_PERCENT + SS_PERCENT + MEDICARE_PERCENT);
 		
-		NET_PAY = (GROSS_PAY - DEDUCTIONS);
-		System.out.println("Net pay:" + "\t" + "\t" + "$" + NET_PAY );
+		netPay = (grossPay - DEDUCTIONS);
+		System.out.println("Net pay:" + "\t" + "\t" + "$" + netPay );
 		
 		System.out.println(" ");
 		System.out.println(" ");
